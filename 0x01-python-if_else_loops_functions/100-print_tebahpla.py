626e45502135e199d18ea2e531dd80a88e547bfc
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-01 23:49:49 +0000

    corrections

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+result = ""
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        print(chr(i), end="")
+        result += chr(i)
     else:
-        print(chr(i).upper(), end="")
+        result += chr(i).upper()
+print(result, end="")
