0ee654274c8d2a4a5f79e39a238bd56a586a0a4d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-02 23:18:32 +0000

    changes to the file for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
     number = len(sys.argv) - 1
     arguments = sys.argv
 
-    if number == 1:
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
         print("{} argument:".format(number))
     else:
         print("{} arguments:".format(number))
+
     for a in range(1, number + 1):
         print("{}: {}".format(a, arguments[a]))
