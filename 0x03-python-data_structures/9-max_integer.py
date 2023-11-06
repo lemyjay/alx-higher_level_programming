ef3b13e5f9c56a2ef3fcda6fb2e0b667275f9c4b
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-06 10:50:39 +0000

    corrections to file of task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    result = 0
+    result = my_list[0]
     for i in my_list:
         if i > result:
             result = i
