a570e7ce897a51893fc858d4bab08848a9eb82b1
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-09 15:36:22 +0000

    correction to task file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,11 +4,12 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     key_list = []
+    if len(list(a_dictionary.items())) == 1:
+        a, b = a_dictionary.items()
+        return b
+    elif len(list(a_dictionary.items())) == 0:
+        return 0
     for x, y in a_dictionary.items():
         key_list.append(x)
     key_list.sort()
-    if len(key_list) == 1:
-        return key_list[0]
-    elif len(key_list) == 0:
-        return None
     return key_list[-1]
