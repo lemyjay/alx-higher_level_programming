b593368e6586e63fef089509490b8ff47ed7ffa1
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-20 15:58:12 +0000

    the file for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-print("something")
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        for a in my_list:
+            if i == x:
+                break
+            print("{}".format(a), end="")
+            i += 1
+        print()
+        return i
+    except Exception as e:
+        print(e)
