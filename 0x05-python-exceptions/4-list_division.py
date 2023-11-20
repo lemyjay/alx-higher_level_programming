36b526b866fce149294526a8db163c09d93ee31f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-20 21:33:48 +0000

    pycodestyle

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     final = []
-    
+
     for i in range(list_length):
         try:
             final.append(my_list_1[i] / my_list_2[i])
