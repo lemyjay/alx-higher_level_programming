f3f13d29b62dce6841b3a8ac48566404d6c06911
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-30 11:18:20 +0000

    the files for task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    A funtion that adds 2 integers.
+
+    Prototype:
+    def add_integer(a, b=98)
+
+    Args:
+        a (int or float): the first integer
+        b (int or float, default: 98): the second integer
+
+    Returns:
+        int: The sum of the two integers
+
+    Raises:
+        TypeError: if a or b is not an integer or a float.
+    """
+    if not isinstance(a, (float, int)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (float, int)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
