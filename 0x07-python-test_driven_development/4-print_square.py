62282c129d4e2efde2783fc5330ebf52e8a30785
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-30 15:51:21 +0000

    the files to task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,4 +5,24 @@ Print square with #
 
 def print_square(size):
     """
+    A function that prints a square with the character #
     
+    Prototype:
+        def print_square(size):
+    
+    Args:
+        size (int): The size length of the square
+    
+    Raises:
+        TypeError: if size is not an integer
+                   if size is a float and less than 0
+        ValueError: if size is less than 0
+    """
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+    if isinstance(size, int) and size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in size:
+        print("#" * int(size))
