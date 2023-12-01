2fe77d20ee158d4255d66860a1a0ea5b6322d48a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-01 21:14:21 +0000

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,10 +12,29 @@ class TestMaxInteger(unittest.TestCase):
     This the class for testing the max_integer function using
     the unittest module
     """
-    def test_empty(self):
-        self.assertEqual(max_integer(), None)
+    def test_regular_list(self):
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
 
-    def test_max(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_reversed_list(self):
+        result = max_integer([1, 3, 4, 2])
+        self.assertEqual(result, 4)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer())
+    
+    def test_positive_numbers(self):
+        result = max_integer([10, 5, 8, 3])
+        self.assertEqual(result, 10)
+    
+    def test_negative_numbers(self):
+        result = max_integer([-5, -8, -3, -1])
+        self.assertEqual(result, -1)
+
+    def test_single_numbers(self):
         self.assertEqual(max_integer([-8595]), -8595)
+        self.assertEqual(max_integer([8595]), 8595)
+
+    def test_mixed_numbers(self):
         self.assertEqual(max_integer([0, -1, -89]), 0)
+        self.assertEqual(max_integer([-5, 0, 3, -1]), 3)
