ced598b877631ee2064fe08611769ba7b14ce624
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-01 20:59:54 +0000

    fixed some syntax errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,19 +7,18 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unittest.TestCase)
+class TestMaxInteger(unittest.TestCase):
     """
     This the class for testing the max_integer function using
     the unittest module
     """
     def test_empty(self):
         self.assertEqual(max_integer(), None)
-    
+
     def test_max(self):
         self.assertEqual([1, 2, 3, 4], 4)
         self.assertEqual([-8595], -8595)
         self.assertEqual([0, -1, -89], 0)
 
     def test_elements(self):
-        self.assertRaises (TypeError, ["s", 5, 6])
-
+        self.assertRaises(TypeError, ["s", 5, 6])
