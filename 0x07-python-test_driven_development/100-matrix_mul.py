80426eca0b4e6dcd6fcc9414aea7c91674efae15
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-02 00:06:49 +0000

    corrections

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -7,23 +7,25 @@ Matrix Multiplication
 def matrix_mul(m_a, m_b):
     """
     A function that multiplies 2 matrices
-    
+
     Prototype:
         def matrix_mul(m_a, m_b):
-    
+
     Args:
         m_a (list): a list of lists of integers or floats
         m_b (list): a list of lists of integers or floats
-    
+
     Returns:
         A new matrix as the result of the multiplication
-    
+
     Raises:
         TypeError: - if m_a or m_b is not a list
                    - if m_a or m_b is not a list of lists
-                   - if m_a or m_b is not a rectangle (all rows should be of the same size)
-                   - if one element of those list of lists is not an integer or float
-        
+                   - if m_a or m_b is not a rectangle: all rows should
+                     be of the same size)
+                   - if one element of those list of lists is not an integer
+                     or float
+
         ValueError: - if m_a or m_b is empty
                     - if m_a and m_b can't be multiplied
     """
@@ -43,14 +45,14 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a should contain only integers or floats")
     if any(not isinstance(j, (int, float)) for i in m_b for j in i):
         raise TypeError("m_b should contain only integers or floats")
-    
+
     row_a = len(m_a[0])
     row_b = len(m_b[0])
     if any(len(i) != row_a for i in m_a):
         raise TypeError("each row of m_a must be of the same size")
     if any(len(i) != row_b for i in m_b):
         raise TypeError("each row of m_b must be of the same size")
-    
+
     # For the multiplication of two matrices to be possible, the number of
     # columns of matrix A should be equal to the number of rows in matrix B
     num_of_colums_matA = len(m_a[0])
