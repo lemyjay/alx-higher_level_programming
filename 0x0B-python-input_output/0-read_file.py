694a90ffdf8e0086e2e48202234585f4861f6990
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-05 10:05:44 +0000

    permissions and pycode style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,10 +7,10 @@ Read file
 def read_file(filename=""):
     """
     A function that reads a text file(UTF8) and prints it to stdout
-    
+
     Prototype:
         def read_file(filename=""):
-    
+
     Args:
         filename (str): The name of the file. Default is an empty string
     """
