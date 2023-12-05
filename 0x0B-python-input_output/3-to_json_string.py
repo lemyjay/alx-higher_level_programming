d019b5a1d3750f17eb3e1ecbe2f866dbae7800db
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-05 11:43:27 +0000

    the files to task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+To JSON string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    A function that returns the JSON representation of an object (string)
+    
+    Prototype:
+        def to_json_string(my_obj):
+    
+    Args:
+        my_obj: The object to be serialized to a JSON-formatted string.
+    
+    Returns:
+        str: The JSON representation of an object (string)
+    """
+    return json.dump(my_obj)
