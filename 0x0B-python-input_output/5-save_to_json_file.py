0c35e51234f820db790c76fd9353b0b0500105bc
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-05 12:49:07 +0000

    pycodestyle

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,7 +7,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    A function that writes an Object to a text file, using a JSON representation
+    A function that writes an Object to a text file, using a JSON
+    representation
 
     Prototype:
         def save_to_json_file(my_obj, filename):
