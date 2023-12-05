b531a7f9027d5e5cb32b88e435fec7407834816c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-05 13:14:55 +0000

    some minor changes

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -16,4 +16,4 @@ def load_from_json_file(filename):
         filename (str): The name of the JSON file
     """
     with open(filename, 'r', encoding='utf-8') as f:
-        json.loads(f.read())
+        return json.loads(f.read())
