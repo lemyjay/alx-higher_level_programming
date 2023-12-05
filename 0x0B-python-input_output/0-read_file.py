5bdfbc78018143d2aa61f727c47be0f2a1c027ee
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-05 12:12:17 +0000

    minor changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,4 +15,4 @@ def read_file(filename=""):
         filename (str): The name of the file. Default is an empty string
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read() end="")
