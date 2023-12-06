4f7809c930d35a0786b9f37bd0750f21e49513e4
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 11:17:16 +0000

    the files to task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Only sub class of
+'''
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if the object is an instance of a class that inherited (directly or indirectly)
+    from the specified class; otherwise, return False.
+
+    Prototype:
+        def inherits_from(obj, a_class):
+
+    Args:
+        obj: The object to check.
+        a_class: The specified class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of a class that inherited from the specified class; False otherwise.
+    """
+    return type(obj) is type(a_class)
