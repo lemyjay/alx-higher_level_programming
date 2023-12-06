25b79ab39aa5c02fc65d3a73c6229d7e0d5da14f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 13:16:58 +0000

    the files to task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Improve Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+
+    Public Methods:
+        def area(self): Raises an Exception with the message 'area() is not implemented.'
+    """
+    def area(self):
+        """Raises an Exception with the message 'area() is not implemented.'"""
+        raise Exception('area() is not implemented')
