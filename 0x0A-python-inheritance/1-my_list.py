7240fff4d4d1022f818b5f1e90a641c6d51dc206
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 09:36:01 +0000

    The files to task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+My list
+'''
+
+
+class MyList(list):
+    """
+    A class that inherits from list.
+
+    Public Methods:
+        def print_sorted(self): Prints the list, but sorted in ascending order.
+
+    Assumptions:
+        - All elements of the list will be of type int.
+    """
+    def pint_sorted(self):
+        # Prints the list but in ascending order
+        self.sort()
