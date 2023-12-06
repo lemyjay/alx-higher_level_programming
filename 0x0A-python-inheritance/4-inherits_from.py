6cc52cf41ba5113e307fc94dd42d00364d8a6ee8
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 13:09:15 +0000

    corrections

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -21,4 +21,4 @@ def inherits_from(obj, a_class):
         bool: True if the object is an instance of a class that inherited
         from the specified class; False otherwise.
     """
-    return issubclass(type(obj), a_class)
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
