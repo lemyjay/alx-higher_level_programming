01710a930e24f2689c86abe8d154b9c4cf7a9b0d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 16:00:44 +0000

    some minor changes

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,9 +25,9 @@ class Rectangle(BaseGeometry):
             TypeError: If width or height is not an integer.
             ValueError: If width or height is less than or equal to 0.
         """
-        BaseGeometry.integer_validator(self, 'width', width)
+        super().integer_validator(self, 'width', width)
         self.__width = width
-        BaseGeometry.integer_validator(self, 'height', height)
+        super().integer_validator(self, 'height', height)
         self.__height = height
 
     def area(self):
