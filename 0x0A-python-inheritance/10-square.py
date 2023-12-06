e86112a03e8565c0e27475f382abee22553977e6
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-06 15:57:50 +0000

    the file to task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Square #1
+'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square, inheriting from Rectangle.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
