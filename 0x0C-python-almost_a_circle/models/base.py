0f9576331cd1582ec37bc7b318c61e3ec7994c0a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-12-08 11:10:11 +0000

    the files to task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Base class
+"""
+
+
+class Base:
+    """
+    The Base class is the foundation for all other classes in this project.
+    It manages the 'id' attribute, ensuring its proper assignment based on the provided argument.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to keep track of the number of objects created.
+
+    Methods:
+        __init__(self, id=None): The class constructor.
+            If 'id' is not None, assign it to the public instance attribute 'id'.
+            Otherwise, increment __nb_objects and assign the new value to 'id'.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = id
