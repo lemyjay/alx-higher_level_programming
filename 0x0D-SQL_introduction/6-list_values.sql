28bdb7ac7440f98edd7525bfeffe98034d7c5433
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-16 20:29:38 +0000

    the file to task 7 and correction to task 6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- Task 06: List all in table
 -- This script lists all rows of the table first_table from the database hbtn_0c_0 in the MySQL server.
 SELECT *
-FROM hbtn_0c_0.first_table
+FROM hbtn_0c_0.first_table;
