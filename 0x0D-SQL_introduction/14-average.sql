d896e1cc51c25d9a4bf5c1635bd29e6dd5335c9a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-16 21:18:29 +0000

    the file to task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Task 14: Average
+-- This script computes the score average of all records in the table second_table of the database hbtn_0c_0 in the MySQL server.
+SELECT AVG(score) FROM second_table;
