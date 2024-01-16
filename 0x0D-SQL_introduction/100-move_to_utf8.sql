e401c7d0dc09a2017de6725fb9934207e34a9fe0
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-16 21:47:55 +0000

    the file to task 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- Task 17: Go to UTF8
+-- This script converts hbtn_0c_0 database to UTF8(utf8mb4, collate utf8mb4_unicode_ci) in the MySQL server.
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
+-- Convert the table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the 'name' field to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
