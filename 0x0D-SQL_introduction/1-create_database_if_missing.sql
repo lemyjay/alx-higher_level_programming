9de719e498bb69e0141b954d703cc4a8d1ae6b27
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-16 16:49:15 +0000

    the file to task 2 and some modifications to task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
 -- Task 0: Create a database
 -- This query creates the database hbtn_0c_0 in the MYSQL server
+-- The script shouldn't fail if hbtn_0c_0 doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Task 0: Delete a database
+-- This query deletes the database hbtn_0c_0 in the MYSQL server
+-- The script should not fail if hbtn_0c_0 doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
