e04be3c28cb6b801d5b9a17c7924dfcb2c38a991
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-18 15:15:50 +0000

    the file for task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- Script to list privileges for users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Script that creates the MySQL user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
