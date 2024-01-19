40d550e805054be755022793738555825f24a200
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-19 02:08:12 +0000

    the file for task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+--  A script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT ts.title
+FROM tv_genres AS tg, tv_show_genres AS tsg, tv_shows AS ts
+WHERE tg.name = 'Comedy' AND ts.id = tsg.show_id AND tg.id = tsg.genre_id
+ORDER BY ts.title;
