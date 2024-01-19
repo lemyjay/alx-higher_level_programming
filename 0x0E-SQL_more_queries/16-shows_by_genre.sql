608d307ef03c0075648cea1901a23601d2a07bad
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-19 02:24:10 +0000

    corrections to the file of task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
 --  A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT ts.title, tg.name
-FROM tv_genres AS tg, tv_show_genres AS tsg, tv_shows AS ts
-WHERE ts.id = tsg.show_id AND tg.id = tsg.genre_id
+FROM tv_genres AS tg 
+JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
+RIGHT JOIN tv_shows AS ts ON ts.id = tsg.show_id
 ORDER BY ts.title ASC, tg.name ASC;
