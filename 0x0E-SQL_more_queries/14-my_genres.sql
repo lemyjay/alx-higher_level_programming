144eeb1c6149e40ebdb8147ce3a186a268374adf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-19 01:24:58 +0000

    the file to task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+--  A script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tg.name
+FROM tv_genres AS tg, tv_show_genres AS tsg, tv_shows AS ts
+WHERE ts.title = 'Dexter' AND ts.id = tsg.show_id AND tg.id = tsg.genre_id
+ORDER BY tg.name;
