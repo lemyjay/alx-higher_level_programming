b6df9769b2af074f3558cbf6cfe2a2001c205972
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-12 16:19:02 +0000

    semistandard coding style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg1 = parseInt(process.argv[2]);
 
 if (arg1) {
-  console.log('My number:', arg1)
+  console.log('My number:', arg1);
 } else {
-    console.log('Not a number')
+  console.log('Not a number');
 }
