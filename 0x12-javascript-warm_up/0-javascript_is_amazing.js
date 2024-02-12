58d3ee28140700d263607613d74198186caa326f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-12 14:03:32 +0000

    the implementation of task 0.

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// A script that "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+# 0x12. JavaScript - Warm up
+
+This directory contains scripts on JavaScript
