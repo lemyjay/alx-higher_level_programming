d312c742a0fc13137e5edeace373d114d00840e5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-12 18:05:52 +0000

    the file to task 6"

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// A script that prints 3 lines, but the lines would be in an array.
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
