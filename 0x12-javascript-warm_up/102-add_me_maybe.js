67e025976c4f84c6d1bd9a978616b1edf2124b3a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-12 19:43:34 +0000

    the file for task 16

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A function that increments and calls a function.
+function addMeMaybe(number, theFunction) {
+  number++;
+  theFunction(number);
+}
+
+module.exports = { addMeMaybe };
