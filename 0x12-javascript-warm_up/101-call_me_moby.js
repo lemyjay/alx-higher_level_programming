c24275bd19dc63ee75948d9420147ca451f93637
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-12 19:29:20 +0000

    semistandard coding style

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // A function that executes x times a function.
-function callMeMoby(x, theFunction) {
+function callMeMoby (x, theFunction) {
   if (x > 0) {
     theFunction();
     callMeMoby(x - 1, theFunction);
