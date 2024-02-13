c28bc4b6d28d87d5f1f9e3cea5d3222728dfbce7
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-13 13:35:27 +0000

    the file to task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+
+    for (let j = 0; j < this.width; j++) {
+      line += 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
