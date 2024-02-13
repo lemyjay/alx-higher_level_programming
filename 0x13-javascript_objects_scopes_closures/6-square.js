ad96f60d14a4c5ecccd8f894aa7eaecf0fe16f97
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-13 14:12:05 +0000

    the file to task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  constructor (size) {
+    super (size);
+  }
+
+  charPrint (c) {
+    let line = '';
+
+    if (!c) {
+      c = 'X';
+    }
+
+    for (let j = 0; j < this.width; j++) {
+      line += c;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+}
+
+module.exports = Square;
