dfc8d864fb4e25f137a560233cc3dc2b10aeb2ee
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-16 23:51:35 +0000

    included the minor details for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,9 +16,11 @@ if __name__ == '__main__':
     try:
         db = MySQLdb.connect(
             host='localhost',
+            port=3306,
             user=username,
             passwd=password,
-            db=database
+            db='hbtn_0e_0_usa',
+            charset='utf8'
         )
         cursor = db.cursor()
     except MySQLdb.Error as e:
