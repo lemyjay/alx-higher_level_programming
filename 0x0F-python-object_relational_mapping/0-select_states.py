fdeee2e344a51be2a87ec2f9475505d6f4e24138
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-16 17:03:27 +0000

    filling the files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+A script that lists all states from the database hhbtn_0e_0_usa
+'''
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL database
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            username=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
+        sys.exit(1)
+    
+    # Execute SQL query to select all states
+    try:
+        cursor.execute('SELECT * FROM states ORDER BY id ASC')
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    
+    # Display results
+    for state in states:
+        print(state)
+    
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
