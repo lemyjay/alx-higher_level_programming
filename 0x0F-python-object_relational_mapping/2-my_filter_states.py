a91ee70e4be3aa8b90757425f3c4ffe3a61378b2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-17 22:09:37 +0000

    the implementation of task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+'''
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL database
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset='utf8'
+        )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    # Execute SQL query to select all states
+    try:
+        query = '\
+        SELECT * FROM states\
+        WHERE name = "{}"\
+        ORDER BY id ASC'.format(state_name)
+        cursor.execute(query)
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    # Display results
+    for id, state in states:
+        print("({}, '{}')". format(id, state))
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
