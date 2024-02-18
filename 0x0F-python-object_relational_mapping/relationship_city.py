0c39eaf12bf385dd1775de94f0e694a2df8a39bf
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-18 23:43:27 +0000

    the files to task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Module that contains the class definition of a City.
+'''
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    Class that inherits from Base to link to the MySQL table cities.
+
+    Attributes:
+        - id: An auto-generated unique integer identifier for the city.
+        - name: A string representing the name of the city, maximum
+                length of 128 characters.
+        - state_id: An integer representing the foreign key to states.id.
+        - state: A relationship attribute, representing the State object
+                linked to this city.
+    """
+    __tablename__ = 'cities'
+    id = Column(
+            Integer, primary_key=True,
+            autoincrement=True, nullable=False
+            )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
