0d41be96ead91d16e7a9abb1b2f5fc759b46d8ec
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-18 15:33:16 +0000

    the file for task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+A script that changes the name of a State object from the database hbtn_0e_6_usa
+'''
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                username, password, database, pool_pre_ping=True)
+            )
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Querying for the State instance object
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.add(state)
+        session.commit()
+
+    session.close()
