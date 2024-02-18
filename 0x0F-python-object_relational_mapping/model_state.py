307f2fab735e7c9abe5ecdbec2788ba911860195
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-18 13:36:44 +0000

    docstring

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,13 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class representing a state in a database.
+
+    Attributes:
+    - id: An auto-generated unique integer identifier for the state.
+    - name: A string representing the name of the state, maximum length of 128 characters.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
