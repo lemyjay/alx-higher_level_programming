2e02eb0161d0848a61a0ad0604ab93eb4342ec28
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-19 14:04:39 +0000

    some changes

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 '''
-Module that defines the relationship of the city class
+Module that contains the class definition of a City.
 '''
 
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base, State
+from model_state import Base
 
 
 class City(Base):
     """
-    Class representing the cities table.
-    Linked to MySQL table "cities"
+    Class that inherits from Base to link to the MySQL table cities.
+
+    Attributes:
+        - id: An auto-generated unique integer identifier for the city.
+        - name: A string representing the name of the city, maximum
+                length of 128 characters.
+        - state_id: An integer representing the foreign key to states.id.
+        - state: A relationship attribute, representing the State object
+                linked to this city.
     """
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(
+            Integer, primary_key=True,
+            autoincrement=True, nullable=False
+            )
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 '''
-Module that defines the relationship of the state class
+Module that contains the class definition of a State and an instance
+Base = declarative_base()
 '''
 
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
-# Creating an instance of declarative_base
 Base = declarative_base()
 
 
 class State(Base):
     """
-    Class representing the states table.
-    Linked to MySQL table "states"
+    Class representing a state in a database.
+
+    Attributes:
+    - id: An auto-generated unique integer identifier for the state.
+    - name: A string representing the name of the state, maximum
+            length of 128 characters.
     """
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
