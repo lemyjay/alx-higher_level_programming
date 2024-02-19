6c1cfed5a959e37635737cf05efa08e6f8bf449c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-19 12:27:43 +0000

    some minor changes

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,7 +8,7 @@ from the database hbtn_0e_100_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+from relationship_state import State, Base
 from relationship_city import City
 
 
