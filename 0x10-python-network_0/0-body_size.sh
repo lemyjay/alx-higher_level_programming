4b884a4f285bd7af723224b3a6f7282a2d79e52d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-29 15:53:07 +0000

    corrections made

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,15 +2,4 @@
 # A Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body response.
 
-touch /tmp/response_body
-curl -s -o /tmp/response_body "$1"
-
-# Use the "wc" command to count the number of bytes in the response body
-# The "-c" option specifies that we want to count bytes
-response_size=$(wc -c < /tmp/response_body)
-
-# Print the size of the response body in bytes
-echo $response_size
-
-# Remove the temporary file
-rm /tmp/response_body
+curl -sI "$1" | grep "Content-Lenght" | cut -d " " -f 2
