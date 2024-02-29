4656a0e7745fab93fc3e66e40efbf61edc4afcc6
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-29 23:31:28 +0000

    the files for the rest of the tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Post Request
-curl -sLX PUT -d 'user_id=98' -H 'Origin:School' "0.0.0.0:5000/catch_me"
+# a Bash script that makes a request to 0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=98" -L -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
