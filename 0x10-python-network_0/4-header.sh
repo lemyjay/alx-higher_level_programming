2cb558bcba088993e3831078c6904e3c32d42d1c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-29 18:04:49 +0000

    some corrections

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL with the X-School-User-Id header and display the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sHX GET "X-School-User-Id: 98" "$1"
