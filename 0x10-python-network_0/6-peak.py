3a6421a23cefd68b5d7b70ca68e578dc300e444e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-29 21:54:33 +0000

    trying a different way

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-'''A function that finds the peak in a given list of integers'''
+
+""" Module to find a peak in a list of unsorted integers.
+Going to utilise a binary search approach.
+"""
 
 
 def find_peak(list_of_integers):
-    # Finds the peak in a given list of integers.
-
-    # Check if the list is empty
+    """Function to find peak in list of integers.
+    """
+    # check if list is empty
     if not list_of_integers:
         return None
 
-    arr = list_of_integers
-    length = len(arr)
-    # Check if the list has only one element
-    if length == 1:
-        return arr[0]
+    # initialize low and high indices
+    low, high = 0, len(list_of_integers) - 1
 
-    # Check if the first element is a peak
-    if arr[0] >= arr[1] and length == 2:
-        return arr[0]
+    # Binary search loop
+    while low < high:
+        # calculate the middle index
+        mid = (low + high) // 2
+        # check if current element is greater than the next element
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # update high to mid, if true
+            high = mid
+        else:
+            # update low to mid, if false
+            low = mid + 1
 
-    # Iterate through the list and check if the current element is a peak
-    for i in range(length):
-        if i == 0:
-            arr[0] >= arr[1]
-            return arr[0]
-        if arr[i - 1] <= arr[i] and arr[i + 1] <= arr[i]:
-            return(arr[i])
-
-    # Check if the last element is a peak
-    if arr[-1] >= arr[-2]:
-        return arr[-1]
-    elif arr[0] >= arr[1]:
-        return arr[0]
-
-    # If no peak is found, return None
-    return None
+    # return element at final low index (represents the peak, I think)
+    # Complexity: O(log(n))
+    return list_of_integers[low]
