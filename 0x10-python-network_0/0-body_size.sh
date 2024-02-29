4c80d808ad9e56e45569c516476412e6afc6c6e2
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-29 15:59:55 +0000

    finally

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # A Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body response.
 
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
+curl -sI "$1" | grep "Content-Length" | cut -d" " -f 2
