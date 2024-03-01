b84ede5f2b19682f549b57bbbbc129965103382d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-01 23:27:20 +0000

    the implementation of task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+A Python script that takes in a URL, sends a request to the
+URL and displays the body of the response (decoded in utf-8).
+'''
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    # Fetching the response
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        # e is of the form 'HTTP Error 404: Not Found'
+        # so using split to get the status code part
+        e = str(e)
+        e = e.split(' ')
+        print("Error code: {}".format(e[2][:3]))
