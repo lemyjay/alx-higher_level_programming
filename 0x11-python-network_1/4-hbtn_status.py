bd6d7d2fa83f755e33b5283e01f385d59b08962c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-01 23:54:26 +0000

    correction to task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,13 +5,12 @@ import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 req = urllib.request.Request(url)
 
-if __name__ == '__main__':
-    # Fetching the response
-    with urllib.request.urlopen(req) as response:
-        data = response.read().decode("utf-8")
+# Fetching the response
+with urllib.request.urlopen(req) as response:
+    data = response.read().decode("utf-8")
 
-        # Getting the type of data
-        print(f'Body response:\n\t- type: {type(data)}')
+    # Getting the type of data
+    print(f'Body response:\n\t- type: {type(data)}')
 
-        # Getting the content of data
-        print(f'\t- content: {data}')
+    # Getting the content of data
+    print(f'\t- content: {data}')
