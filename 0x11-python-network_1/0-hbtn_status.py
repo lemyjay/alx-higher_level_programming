0c1143b33f2344adbdbe853cda43363ea6760161
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-01 16:36:02 +0000

    the implementation of task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''A Python script that fetches https://alx-intranet.hbtn.io/status'''
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+
+    # Getting the type of the content (class 'bytes')
+    print(f'Body response:\n\t- type: {type(data)}')
+
+    # Getting the content (b'OK')
+    print(f'\t- content: {data}')
+
+    # Getting the utf8 content ('OK')
+    print(f'\t- utf8 content: {data.decode("utf-8")}')
