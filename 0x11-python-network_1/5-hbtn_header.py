b44a2d63275206c32dfb62adfc525dd2c7dd4700
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-02 00:42:46 +0000

    some minor changes to task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,8 +7,8 @@ This should be implemented using Request
 import requests
 import sys
 
-# if __name__ == "__main__":
-url = sys.argv[1]
-# Getting the value of the X-Request-Id variable in the dict or headers
-request = requests.get(url).headers['X-Request-Id']
-print(request)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    # Getting the value of the X-Request-Id variable in the dict or headers
+    request = requests.get(url).headers.get('X-Request-Id')
+    print(request)
