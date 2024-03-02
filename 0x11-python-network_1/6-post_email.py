43ca730397c72659b0a7e564dc83124b0cde9085
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-02 00:48:11 +0000

    the implementation of task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+A Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the
+body of the response.
+'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {}
+    value['email'] = email
+    # data = urllib.parse.urlencode(value)
+    # data = data.encode('ascii')
+    request = requests.get(url, value)
+    print(request)
