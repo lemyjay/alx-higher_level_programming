91c82a7b56fced6e16849c418df4893b661fe514
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-02 02:32:27 +0000

    pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     passwd = sys.argv[2]
-    
+
     url = "https://api.github.com/user"
 
     response = requests.get(url, auth=(username, passwd))
