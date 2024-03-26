9979b733638d8d3741ad534fac6baf82d1ec6d8b
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-26 17:24:40 +0000

    some minor changes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,11 +7,11 @@ character “Wedge Antilles” is present.
 const request = require('request');
 const characterId = 18;
 
-request(process.argv[2], (err, response) => {
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    const filmsData = JSON.parse(response.body).results;
+    const filmsData = JSON.parse(body).results;
     const moviesWithWedge = filmsData.filter((film) =>
       film.characters.includes(
         `https://swapi-api.alx-tools.com/api/people/${characterId}/`
